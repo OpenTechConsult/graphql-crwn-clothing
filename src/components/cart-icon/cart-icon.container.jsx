954f5376acf8b5aaa04d95a16837066d465b4b69
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-07-28 11:10:34 +0000

    Fix Bug : Unhandled Rejection (TypeError): Cannot read property 'itemCount' of undefined #1

--- src/components/cart-icon/cart-icon.container.jsx
+++ src/components/cart-icon/cart-icon.container.jsx
@@ -16,15 +16,17 @@ const GET_ITEM_COUNT = gql`
   }
 `;
 
-const CartIconContainer = ({ toggleCartHidden, data: { itemCount } }) => (
+const CartIconContainer = () => (
   <Query query={GET_ITEM_COUNT}>
     {
-      ({ data: { itemCount } }) =>
-      (<Mutation mutation={TOGGLE_CART_HIDDEN}>
-      {
-        toggleCartHidden => <CartIcon toggleCartHidden={toggleCartHidden} itemCount={itemCount} />
-      }
-    </Mutation>)}
+      ({ data: { itemCount } }) => (
+        <Mutation mutation={TOGGLE_CART_HIDDEN}>
+          {
+            toggleCartHidden => <CartIcon toggleCartHidden={toggleCartHidden} itemCount={itemCount} />
+          }
+        </Mutation>
+        )
+    }
   </Query>
 );
 
